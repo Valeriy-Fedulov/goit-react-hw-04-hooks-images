1db707f3e92218fc2a91d1e17485bd743986d5ec
vofm <valerafm.vofm@gmail.com>
2021-11-15 19:02:44 +0200

    Fix axios defaults

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
 function fetchImage(query, page) {
-  return axios.get(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=8645843-73f0b565a99dd2126325d1c4b&image_type=photo&orientation=horizontal&per_page=12`
-  );
+  axios.defaults.baseURL = "https://pixabay.com/api";
+  axios.defaults.params = {
+    q: query,
+    page: page,
+    key: "8645843-73f0b565a99dd2126325d1c4b",
+    image_type: "photo",
+    orientation: "horizontal",
+    per_page: 12,
+  };
+  return axios.get();
 }
 
 const api = { fetchImage };
