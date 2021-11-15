445918d11342135f8ef59ae68dc8c64461c1aa36
vofm <valerafm.vofm@gmail.com>
2021-11-15 19:23:47 +0200

    Fix

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -8,6 +8,7 @@ function fetchImage(query, page) {
     key: "8645843-73f0b565a99dd2126325d1c4b",
     image_type: "photo",
     orientation: "horizontal",
+    safesearch: true,
     per_page: 12,
   };
   return axios.get();
